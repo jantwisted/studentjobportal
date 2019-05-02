37425eb3d65267f7226904254b504385e803853c
Janith Perera <janith@member.fsf.org>
2019-05-02 12:18:16 +0200

    PORT env added

--- app.py
+++ app.py
@@ -93,4 +93,4 @@ def logout():
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(debug=True,host='0.0.0.0', port=int(os.environ.get('KEY_THAT_MIGHT_EXIST')))
+    app.run(debug=True, port=int(os.environ.get('PORT')))
