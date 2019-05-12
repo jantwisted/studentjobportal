2b1faffd0b43f06838976cb2129eef550e2a8828
Janith Perera <janith@member.fsf.org>
2019-05-12 14:55:15 +0200

    user registration added

--- app.py
+++ app.py
@@ -12,11 +12,13 @@ app.config['GOOGLEMAPS_KEY'] = "AIzaSyCN5UwzussKx2SSNjo-qJla0f3aGr-KnmQ"
 app.secret_key = os.urandom(12)
 GoogleMaps(app)
 
-api_url = 'https://studentjobengine.herokuapp.com'
+api_url = 'http://127.0.0.1:8080'
 
 jwt_token = ''
 
-def get_all_jobs():
+def get_all_jobs(latitude, longitude):
+    print(latitude)
+    print(longitude)
     url = api_url+'/jobs/all'
     print(url)
     headers = {'Authorization': 'Bearer '+jwt_token}
@@ -41,11 +43,11 @@ def build_marker(jobs_list):
 
 
 @app.route('/')
-def home():
+def home(latitude='', longitude=''):
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        jobs_list = get_all_jobs()
+        jobs_list = get_all_jobs(latitude, longitude)
         markers = build_marker(jobs_list)
         sndmap = Map(
             identifier="sndmap",
@@ -63,8 +65,11 @@ def home():
 @app.route('/login', methods=['POST'])
 def do_admin_login():
     user_details = {}
+    print(request.form)
     user_name = request.form['username']
     user_password = request.form['password']
+    current_latitude = request.form['latitude']
+    current_longitude = request.form['longitude']
     user_details['UserName'] = user_name
     user_details['Password'] = user_password
     url = api_url+'/users/login'
@@ -82,7 +87,45 @@ def do_admin_login():
             flash('wrong password!')
     except jwt.DecodeError:
         flash('wrong credentials!')
+    return home(current_latitude, current_longitude)
+
+@app.route("/showregister")
+def showregister():
+    return render_template('register.html')
+
+@app.route("/register", methods=['POST'])
+def register():
+    user_details = {}
+    print(request.form)
+    user_name = request.form['userName']
+    user_password = request.form['inputPassword']
+    user_firstname = request.form['firstName']
+    user_lastname = request.form['lastName']
+    user_email = request.form['inputEmail']
+    user_type = "student"
+    user_status = "1"
+    user_details['UserName'] = user_name
+    user_details['Password'] = user_password
+    user_details['FirstName'] = user_firstname
+    user_details['LastName'] = user_lastname
+    user_details['UserType'] = user_type
+    user_details['UserStatus'] = user_status
+    user_details['Email'] = user_email
+    url = api_url+'/users/add'
+    body = user_details
+    print(body)
+    headers = {'content-type': 'application/json'}
+    response = requests.post(url, data=json.dumps(body), headers=headers)
+    response_dict = json.loads(response.text)
+    if response_dict['Action'] == 'register':
+        if response_dict['Result'] == 'true':
+            flash('successfully registered')
+        else:
+            flash('something went wrong')
+    else:
+        flash('wrong msg type')
     return home()
+
 
 
 @app.route("/logout")
